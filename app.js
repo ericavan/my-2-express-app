9a474aa00f78614b2f2b1bc7c05c13d16e801136
ericavan <elv9@georgetown.edu>
2021-05-03 16:32:58 -0400

    Display Flash Messages - not working

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config(); // new line 1
 
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session') // around line 7
+var flash = require('express-flash-messages') // around line 8
 
 var expressLayouts = require('express-ejs-layouts'); // around line 6
 
@@ -13,6 +14,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var stocksRouter = require('./routes/stocks') // around line 14
 var app = express();
+
+var SESSION_SECRET = process.env.SESSION_SECRET || "super secret" // around line 17 (before app initialization)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +26,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// around line 29 (after cookie parser)
+app.use(session({
+  cookie: { maxAge: 60000},
+  secret: SESSION_SECRET,
+  name: 'stocks-app-session',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

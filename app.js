e3185e8c8d6078d0b6dd3fe0e9091ad4fc19f922
ericavan <elv9@georgetown.edu>
2021-05-05 14:14:41 -0400

    Fixed style issues with app.js file

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 // around line 29 (after cookie parser)
 app.use(session({
